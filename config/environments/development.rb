e5cda795e57c6a2a559ba5e538c3a90064f01d6b
nozomusato <nojyamaru.pikkoro.morori@gmail.com>
2020-03-31 20:46:38 +0000

    レイアウト6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  
+  config.public_file_server.enabled = true # true に変更
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
