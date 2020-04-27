309e88381bdd16ae8f2d0ff2126e04f77bfac4ae
nozomusato <nojyamaru.pikkoro.morori@gmail.com>
2020-04-27 17:27:28 +0000

    ユーザー一覧adminのみ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy,:logs,:logs_month,:logs_day,:start,:csv_dl]
   before_action :logged_in_user, only: [:index, :show,:start,:places, :logs, :logs_month, :logs_day,:csv_dl,:edit_action,:edit, :update]
   before_action :correct_user, only: [:show,:start,:logs_month,:logs_day,:edit_action,:edit, :update]
-  before_action :admin_user, only: [:destroy]
+  before_action :admin_user, only: [:destroy,:index]
 
   def index
     @users = User.all.page(params[:page]).search(params[:search])
